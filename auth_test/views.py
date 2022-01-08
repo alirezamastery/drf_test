b5e7845ac796458047edadc66c70884dc6622d22
alireza <alireza.mastery@gmail.com>
2022-01-08 14:53:56 +0330

    api schema test

--- auth_test/views.py
+++ auth_test/views.py
@@ -3,6 +3,13 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import serializers
+from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import (
+    extend_schema, extend_schema_view, OpenApiExample, OpenApiParameter
+)
+from drf_spectacular.openapi import OpenApiTypes
+
 from .models import SignUpToken
 
 
@@ -14,8 +21,33 @@ class AllowAnyView(GenericAPIView):
         return Response({'ok': 'ok'}, status.HTTP_200_OK)
 
 
+class TestRequestSerializer(serializers.Serializer):
+    test = serializers.CharField()
+
+
+class TestQuerySerializer(serializers.Serializer):
+    param = serializers.CharField()
+
+
 class VerifySignUp(GenericAPIView):
 
+    @extend_schema(
+        parameters=[
+            TestQuerySerializer,  # serializer fields are converted to parameters
+            OpenApiParameter("nested", TestQuerySerializer),  # serializer object is converted to a parameter
+            OpenApiParameter(
+                "chaikin_param_1",
+                OpenApiTypes.UUID,
+                OpenApiParameter.QUERY,
+                description='chaikin_param_1 description',
+                examples=[OpenApiExample(name='chaikin 1 example', value='ccc')]
+            ),
+            OpenApiParameter("pk", OpenApiTypes.UUID, OpenApiParameter.PATH),  # path variable was overridden
+        ],
+        request=TestRequestSerializer,
+        responses=TestRequestSerializer,
+        # more customizations
+    )
     def get(self, request):
         token = request.GET.get('token')
         print(token)
